c00f9303032df9dc3d41d7105ae9bf5a232f08b4
NiqhtFire <66108534+NiqhtFire@users.noreply.github.com>
2022-10-12 21:05:01 +0300

    V2
    a

---
+++ E-Waste_Day/Assets/Scripts/HighJump.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighJump : MonoBehaviour
+{
+    public Movement movescr;
+    Rigidbody2D rb;
+    public ParticleSystem jdust;
+    public float CDTime = 2;
+    public  float nextUseTime = 0;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (Time.time > nextUseTime){
+        if(Input.GetKeyDown(KeyCode.Q)){
+            rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse); 
+            StartCoroutine(Wait()); 
+            nextUseTime = Time.time + CDTime;   
+            CreateDust();
+        }
+    }
+    }
+    IEnumerator Wait()
+    {
+        
+
+        yield return new WaitForSeconds(0.4f);
+       
+        
+      
+        
+    }
+    void CreateDust()
+    {
+        Debug.Log("kek");
+        jdust.Play();
+    }
+    
+}
